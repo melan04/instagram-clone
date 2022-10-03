daf362bae22769d28e3f3b8d4e4fb91d13593b36
Mélany Condé <melany.conde@gmail.com>
2022-10-03 17:08:59 +0200

    Create profil icon

--- instagram/src/components/Menu.js
+++ instagram/src/components/Menu.js
@@ -1,9 +1,10 @@
 import "../styles/menu.scss";
+import ProfileIcon from "./ProfileIcon";
 
-import {ReactComponent as Home} from "../images/home.svg";
-import {ReactComponent as Inbox} from "../images/inbox.svg";
-import {ReactComponent as Explore} from "../images/explore.svg";
-import {ReactComponent as Notifications} from "../images/notifications.svg";
+import { ReactComponent as Home } from "../images/home.svg";
+import { ReactComponent as Inbox } from "../images/inbox.svg";
+import { ReactComponent as Explore } from "../images/explore.svg";
+import { ReactComponent as Notifications } from "../images/notifications.svg";
 
 function Menu() {
   return <div className="menu">
@@ -11,8 +12,7 @@ function Menu() {
     <Inbox className="icon" />
     <Explore className="icon" />
     <Notifications className="icon" />
-
-
+    <ProfileIcon />
   </div>
 }
 
