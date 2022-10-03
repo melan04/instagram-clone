c99d3971a44535522963d74d43d90b3233f133f6
Mélany Condé <melany.conde@gmail.com>
2022-10-03 16:11:40 +0200

    Add icons menu

--- instagram/src/components/Menu.js
+++ instagram/src/components/Menu.js
@@ -1,5 +1,18 @@
+import "../styles/menu.scss";
+import {ReactComponent as Home} from "../images/home.svg";
+import {ReactComponent as Inbox} from "../images/inbox.svg";
+import {ReactComponent as Explore} from "../images/explore.svg";
+import {ReactComponent as Notifications} from "../images/notifications.svg";
+
 function Menu() {
-  return <div className="menu">menu</div>
+  return <div className="menu">
+    <Home className="icon" />
+    <Inbox className="icon" />
+    <Explore className="icon" />
+    <Notifications className="icon" />
+
+
+  </div>
 }
 
 export default Menu;
